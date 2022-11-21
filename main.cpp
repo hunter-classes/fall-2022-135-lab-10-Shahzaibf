c19c9a156cf92d971a1aae9947adf8643764285d
Shahzaib Fareed <shahzaibfareed1118@gmail.com>
2022-11-21 11:09:21 -0500

    Added Task A to main

--- main.cpp
+++ main.cpp
@@ -2,5 +2,11 @@
 #include <iostream>
 
 int main(){
+    Time early = {5, 30};
+    Time later = {11,20};
+
+    std::cout << minutesSinceMidnight(early) << "\n"; // 330
+    std::cout << minutesUntil(early, later) << "\n"; // 350
+
     return 0;
 }
